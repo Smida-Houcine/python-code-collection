7ebd007e6a13a6c26f6ebbe3356252fd68b12fda
SMIDA Houcine L. <houcine.smida@isggb.rnu.tn>
2026-08-06 14:53:32 +0100

    Enhance input validation and error handling
    Added input validation for user input and improved error handling.

--- recursive_double_factorial.py
+++ recursive_double_factorial.py
@@ -9,18 +9,27 @@ def double_factorial(n):
         return n * double_factorial(n - 2)
 
 # Ask for the value of n
-n = int(input("Enter the value of n: "))
+n_value = input("Enter the value of n (n >= -1): ")
+
+# Check if input is empty
+if len(n_value.strip()) == 0:
+    print("\nError: n must be entered.")
+    exit()
+try:
+    # Convert input to integer
+    n = int(n_value)
+except ValueError:
+    # Display an error message if the input is not an integer
+    print("\nError: n must be an integer.")
+    exit()
 
 # Check that n is valid
 if n < -1:
-    print("\nError: n must be an integer greater than or equal to -1 (n >= -1).")
+    print("\nError: n must be greater than or equal to -1 (n >= -1).")
 else:
     # Calculate and display double factorial
     result = double_factorial(n)
-    if n == -1:
-        print(f"\n({n})!! = {result}")
-    else:
-        print(f"\n{n}!! = {result}")
+    print(f"\n{n}!! = {result}")
 
 """
 Compute the double factorial n!!
